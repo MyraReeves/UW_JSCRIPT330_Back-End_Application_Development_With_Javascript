fdeb45608258b4d54ab5246baed86853fbd18c6e
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-04-09 01:49:35 -0700

    Solves getById() method

--- Class1/daos/items.js
+++ Class1/daos/items.js
@@ -1,17 +1,26 @@
 const uuid = require('uuid');
-
 const itemsModel = require('../models/items');
 
 module.exports = {};
-
 
 module.exports.getAll = () => {
   return itemsModel.items;
 }
 
+/*
+FROM MDN WEB DOCS:
+The findIndex() method returns the index of the first element in an array that satisfies the provided testing function.
+If no elements satisfy the testing function, -1 is returned. 
+The find() method, on the other hand, returns the first element that satisfies the testing function (rather than its index).
+*/
+
+
+// Return an item from the itemsModel array based on its id, using the find() method:
 module.exports.getById = (itemId) => {
-  // TODO: complete
+  const itemToBeFound = itemsModel.items.find(itemToBeFound => itemToBeFound.id === itemId);
+  return itemToBeFound
 }
+
 
 module.exports.deleteById = async (itemId) => {
     // TODO: complete
