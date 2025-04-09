dc41b98f38c0b6c57c678e5fed3883263748e72c
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-04-09 02:29:26 -0700

    Solves updateById method

--- Class1/daos/items.js
+++ Class1/daos/items.js
@@ -15,21 +15,52 @@ The find() method, on the other hand, returns the first element that satisfies t
 */
 
 
-// Return an item from the itemsModel array based on its id, using the find() method:
+/////////////////////////////////////////////////////////////////////////////////////////
+// Return an item from the itemsModel array based on its id, using the find() method: //
+///////////////////////////////////////////////////////////////////////////////////////
 module.exports.getById = (itemId) => {
   const itemToBeFound = itemsModel.items.find(itemToBeFound => itemToBeFound.id === itemId);
   return itemToBeFound
 }
 
 
+
 module.exports.deleteById = async (itemId) => {
     // TODO: complete
 }
 
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+// Replace an existing item in an array with the value of newObj without changing its id: //
+///////////////////////////////////////////////////////////////////////////////////////////
 module.exports.updateById = async (itemId, newObj) => {
-    // TODO: complete
+
+  // Find the index of the item with the given ID
+  const requestedIndex = itemsModel.items.findIndex(item => item.id === itemId);
+    
+  // If the item does not exist, return null
+  if (requestedIndex === -1) {
+    return null;
+  }
+
+  // Find the item located at that index: 
+  const currentValue = itemsModel.items[requestedIndex];
+
+  // Use a spread operator to insert the value of newObj into that item AND ensure that the id remains what it was before:
+  const newValue = { ...currentValue, ...newObj, id: currentValue.id };
+
+  // Replace the item at the requested index with the new value:
+  itemsModel.items[requestedIndex] = newValue;
+
+  return newValue;
 }
 
+
+
+/////////////////////////////////////
+// Create a new item in the array //
+///////////////////////////////////
 module.exports.create = async (item) => {
   const id = uuid.v4();
   const newItem = { ...item, id };
