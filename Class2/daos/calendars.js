0f7a6da4b9d29edfa52f82b1ff596c04c541728b
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-04-12 23:48:51 -0700

    Adds a delete function

--- Class2/daos/calendars.js
+++ Class2/daos/calendars.js
@@ -1,11 +1,14 @@
 const Calendars = require('../models/calendars');
-
 module.exports = {};
-  
+
+
+// This CREATES/POSTS a calendar document in the database:
 module.exports.create = async (name) => {
   return await Calendars.create({ name });
 };
 
+
+// This READS/GETS a calendar from the database:
 module.exports.getById = async (id) => {
   try {
     const calendar = await Calendars.findOne({ _id: id }).lean();
@@ -15,6 +18,8 @@ module.exports.getById = async (id) => {
   }
 };
 
+
+// This UPDATES/PUTS a calendar in the database:
 module.exports.updateById = async (id, newData) => {
   try {
     const calendar = await Calendars.findOneAndUpdate({ _id: id }, newData, { new: true }).lean();
@@ -23,3 +28,9 @@ module.exports.updateById = async (id, newData) => {
     return null;
   }
 };
+
+
+// This DELETES a calendar in the database:
+module.exports.deleteById = async (id) => {
+  return Calendars.findByIdAndDelete( {_id: id} );
+}
