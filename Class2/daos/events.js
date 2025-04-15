0a0ad3cf7a9d46e1469e1229d64566b26da19267
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-04-14 23:51:42 -0700

    Improves readability

--- Class2/daos/events.js
+++ Class2/daos/events.js
@@ -1,9 +1,14 @@
 const Events = require('../models/events');
+module.exports = {};
 
+
+// This CREATES/POSTS an event:
 module.exports.create = async (eventData) => {
   return await Events.create(eventData);
 };
 
+
+// This READS/GETS a single event:
 module.exports.getById = async (id) => {
   try {
     return await Events.findById(id).lean();
@@ -12,10 +17,14 @@ module.exports.getById = async (id) => {
   }
 };
 
+
+// This READS/GETS all of the events:
 module.exports.getAllByCalendarId = async (calendarId) => {
   return await Events.find({ calendarId }).lean();
 };
 
+
+// This UPDATES/PUTS an event:
 module.exports.updateById = async (id, updateData) => {
   try {
     return await Events.findByIdAndUpdate(id, updateData, { new: true }).lean();
@@ -24,6 +33,8 @@ module.exports.updateById = async (id, updateData) => {
   }
 };
 
+
+// This DELETES an event:
 module.exports.removeById = async (id) => {
   try {
     return await Events.findByIdAndDelete(id);
