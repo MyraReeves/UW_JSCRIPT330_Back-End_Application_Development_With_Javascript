85ad75e31aaa85ac2f692aca3add396e10c4aedc
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-04-21 19:38:57 -0700

    Re-orders into CRUD order purely for logic/readability reasons, even though the order doesn't matter for functionality

--- Class3/daos/book.js
+++ Class3/daos/book.js
@@ -1,13 +1,43 @@
 const mongoose = require('mongoose');
-
 const Book = require('../models/book');
-
 module.exports = {};
 
-module.exports.getAll = (page, perPage) => {
-  return Book.find().limit(perPage).skip(perPage*page).lean();
+
+////////////////////////////////////////////////////
+// CREATES a new book entry inside the database: //
+//////////////////////////////////////////////////
+module.exports.create = async (bookData) => {
+  try {
+    const created = await Book.create(bookData);
+    return created;
+  }
+  // Throw a BadDataError message upon validation failure: 
+  catch (e) {
+    if (e.message.includes('validation failed')) {
+      throw new BadDataError(e.message);
+    }
+    throw e;
+  }
+}
+//  Create a unique error type for an invalid book creation:
+class BadDataError extends Error {};
+module.exports.BadDataError = BadDataError;
+
+
+/////////////////////////////////////////////////////////
+// READS/GETS all the info inside the books database: //
+///////////////////////////////////////////////////////
+module.exports.getAll = ({ authorId, page, perPage }) => {
+  // Return all books using the author id:
+  const filter = {};
+  if (authorId && mongoose.Types.ObjectId.isValid(authorId)) {
+    filter.authorId = authorId;
+  }
+  // Return all books using page and perPage:
+  return Book.find(filter).limit(perPage).skip(perPage * page).lean();
 }
 
+// READS/GETS the information about a SINGLE book:
 module.exports.getById = (bookId) => {
   if (!mongoose.Types.ObjectId.isValid(bookId)) {
     return null;
@@ -15,14 +45,10 @@ module.exports.getById = (bookId) => {
   return Book.findOne({ _id: bookId }).lean();
 }
 
-module.exports.deleteById = async (bookId) => {
-  if (!mongoose.Types.ObjectId.isValid(bookId)) {
-    return false;
-  }
-  await Book.deleteOne({ _id: bookId });
-  return true;
-}
 
+////////////////////////////////////////////////////////
+// UPDATES a book inside the database, using its id: //
+//////////////////////////////////////////////////////
 module.exports.updateById = async (bookId, newObj) => {
   if (!mongoose.Types.ObjectId.isValid(bookId)) {
     return false;
@@ -31,17 +57,14 @@ module.exports.updateById = async (bookId, newObj) => {
   return true;
 }
 
-module.exports.create = async (bookData) => {
-  try {
-    const created = await Book.create(bookData);
-    return created;
-  } catch (e) {
-    if (e.message.includes('validation failed')) {
-      throw new BadDataError(e.message);
-    }
-    throw e;
-  }
-}
 
-class BadDataError extends Error {};
-module.exports.BadDataError = BadDataError;
+////////////////////////////////////////////////
+// DELETES a book listing from the database: //
+//////////////////////////////////////////////
+module.exports.deleteById = async (bookId) => {
+  if (!mongoose.Types.ObjectId.isValid(bookId)) {
+    return false;
+  }
+  await Book.deleteOne({ _id: bookId });
+  return true;
+}
