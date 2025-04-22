868590e522d11707947df4735c8a14794ec0715c
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-04-21 18:54:39 -0700

    Adds a note

--- Class3/models/author.js
+++ Class3/models/author.js
@@ -6,5 +6,6 @@ const authorSchema = new mongoose.Schema({
   yearBorn: { type: Number }
 });
 
+// Indexing is not needed for the keys in the authorSchema, since users don't need to search by author gender nor the year an author was born. Per the wording of the test files, searching using the authorId key (which is indexed in the book.js model) can take the place of searching by author name.
 
 module.exports = mongoose.model("authors", authorSchema);
