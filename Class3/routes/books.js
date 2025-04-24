563e1dbfebafd8d9d488c16f08e9bd382f8974e3
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-04-23 19:27:24 -0700

    Adds a route to handle getting books using a search query

--- Class3/routes/books.js
+++ Class3/routes/books.js
@@ -37,9 +37,9 @@ router.post("/", async (req, res, next) => {
 });
 
 
-//////////////////////////////
-// READ data for all books //
-////////////////////////////
+//////////////////////////////////////////////
+// READ data for all books in the database //
+////////////////////////////////////////////
 router.get("/", async (req, res, next) => {
 
   // Set the value of "page" and "perPage" to be their corresponding values inside the query URL:
@@ -69,36 +69,78 @@ router.get("/", async (req, res, next) => {
 });
 
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// READ the books data using the search function -- This handles GET requests such as "/books/search?query=Steelheart" //
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+router.get("/search", async (req, res, next) => {
 
-///////////////////////////////////////////////////
-// READ the data for a single book using its id //
-/////////////////////////////////////////////////
+  // Save the search request's query string inside of a variable:
+  const { query } = req.query;
+
+  // Pass that string into the DAO and return the results in JSON form:
+  try {
+    const results = await bookDAO.search(query);
+    // If the search was successfully performed (even if no result found), return a 200 OK message along with the results (even if that result is an empty array)
+    res.status(200).json(results);
+  } 
+  
+  // Send a 500 error if something goes wrong:
+  catch (e) {
+    res.status(500).send(e.message);
+  }
+});
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// READ the data for a single book using its id -- This handles GET requests such as "/books/102291"  //
+///////////////////////////////////////////////////////////////////////////////////////////////////////
 router.get("/:id", async (req, res, next) => {
 
-  // 
+  // Pass the requested id to the DAO and save the result in a variable called "book":
   const book = await bookDAO.getById(req.params.id);
+
+  // If a matching id is found, return the book data in JSON form:
   if (book) {
     res.json(book);
-  } else {
+  } 
+  // Otherwise, return a 404 error:
+  else {
     res.sendStatus(404);
   }
 });
 
 
-// Update
+////////////////////////////////////////////////////////////////////////////////////////////
+// UPDATE a book's data using its id -- This handles PUT requests such as "/books/10229" //
+//////////////////////////////////////////////////////////////////////////////////////////
 router.put("/:id", async (req, res, next) => {
+
+  // Save into a variable the id that appears in the request URL:
   const bookId = req.params.id;
+
+  // Save into a variable the new data that was sent in the request body
   const book = req.body;
+
+  // If no new data was entered to update, return a 400 error:
   if (!book || JSON.stringify(book) === '{}' ) {
-    res.status(400).send('book is required"');
-  } else {
+    res.status(400).send('book is required');
+  } 
+  
+  // Otherwise, use the DAO to update the book's information. If that's successful, send a 200 message; if not send a 400 error
+  else {
     try {
       const success = await bookDAO.updateById(bookId, book);
       res.sendStatus(success ? 200 : 400); 
-    } catch(e) {
+    } 
+    
+    // If there is a validation error, returns a 400 error:
+    catch(e) {
       if (e instanceof bookDAO.BadDataError) {
         res.status(400).send(e.message);
-      } else {
+      } 
+      // For all other errors, send a 500 error message:
+      else {
         res.status(500).send(e.message);
       }
     }
@@ -106,16 +148,26 @@ router.put("/:id", async (req, res, next) => {
 });
 
 
-// Delete
+////////////////////////////////////////////////////////////////////////////////////////
+// DELETE a book using its id -- This handles DELETE requests such as "/books/12345" //
+//////////////////////////////////////////////////////////////////////////////////////
 router.delete("/:id", async (req, res, next) => {
+
+  // Save into a variable the id that is in the request URL:
   const bookId = req.params.id;
+
+  // Use the DAO to delete the corresponding book:
   try {
     const success = await bookDAO.deleteById(bookId);
+    //  If the book is successfully deleted, return a 200 message. Otherwise, return a 400 error:
     res.sendStatus(success ? 200 : 400);
-  } catch(e) {
+  } 
+  // In the case of any errors, return a 500 error message:
+  catch(e) {
     res.status(500).send(e.message);
   }
 });
+
 
 
 module.exports = router;
