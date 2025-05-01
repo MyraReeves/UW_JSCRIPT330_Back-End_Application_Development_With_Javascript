1b771a2988c3d30029a7fa4bb50ae127321b612d
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-04-30 21:44:39 -0700

    Adds notes routes

--- Class4/server.js
+++ Class4/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mongoose = require('mongoose');
 const authorizationRoutes = require("./routes/auth");
+const notesRoutes = require("./routes/notes");
 const server = express();
 
 server.use(express.json());
 
 server.use('/auth', authorizationRoutes);
+server.use('/notes', notesRoutes);
 
 module.exports = server;
