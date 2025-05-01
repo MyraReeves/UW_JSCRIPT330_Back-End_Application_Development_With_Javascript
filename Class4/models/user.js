4d993d29b5b12b87f320b5c177d1035c480e275e
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-04-30 20:18:55 -0700

    Adds missing attributes and fixes error with plural in model definition

--- Class4/models/user.js
+++ Class4/models/user.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
 
+// Create a schema:
 const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
-  // TODO: Add additional attributes
+  email: { type: String, required: true, unique: true},
+  twoFactorVerification: {type: String},
 });
 
+// Define the model:
+const User = mongoose.model('User', userSchema)
 
-module.exports = mongoose.model("users", userSchema);
+// Export the model:
+module.exports = User;
