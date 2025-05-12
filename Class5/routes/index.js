aea6515ea3e13e81c870fcc04efb2cfb1dcee217
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-05-11 23:50:45 -0700

    Adds authorization middleware function

--- Class5/routes/index.js
+++ Class5/routes/index.js
@@ -1,4 +1,27 @@
 const { Router } = require("express");
 const router = Router();
+const jwt = require('jsonwebtoken');
+
+
+// You'll want to write an `isAuthorized` middleware function that can be reused. It should verify the JWT provided in `req.headers.authorization` and put the decoded value on the `req` object.
+// Define Express middleware:
+const authorizationMiddleware = (req, res, next) => {
+    const authorizationHeader = req.headers.authorization;
+
+    if (!authorizationHeader) {
+        return res.sendStatus(401);
+    }
+
+    const token = authorizationHeader.split(' ')[1];
+
+    try {
+        const user = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = user;
+        next();
+    }
+    catch (error) {
+        return res.sendStatus(401);
+    }
+};
 
 module.exports = router;
