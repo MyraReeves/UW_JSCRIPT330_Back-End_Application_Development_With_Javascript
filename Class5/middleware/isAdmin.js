02603dcbd2b14df2254ed9e1d90e67ec31307fff
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-05-12 20:42:57 -0700

    Makes style of instructional note same as the other middleware file

--- Class5/middleware/isAdmin.js
+++ Class5/middleware/isAdmin.js
@@ -1,4 +1,7 @@
-// INSTRUCTIONS:  You'll want to write an `isAdmin` middleware function that can be reused. If the user making the request is not an admin it should respond with a 403 Forbidden error.
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// INSTRUCTIONS:  You'll want to write an `isAdmin` middleware function that can be reused.      //
+// If the user making the request is not an admin it should respond with a 403 Forbidden error. //
+/////////////////////////////////////////////////////////////////////////////////////////////////
 
 const isAdmin = (req, res, next) => {
     if (req.user && req.user.roles.includes("admin")) {
