901698637dee72a4f107d59d812b73070384419d
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-05-12 23:56:31 -0700

    Refactored routes to use the associated DAO file

--- Class5/routes/items.js
+++ Class5/routes/items.js
@@ -3,17 +3,15 @@ const Item = require("../models/itemModel");
 const isAuthorized = require("../middleware/isAuthorized");
 const isAdmin = require("../middleware/isAdmin");
 const router = express.Router();
+const itemDao = require("../dao/itemDao");
 
 
 /////////////////////////////////////////////////////////////////
 // CREATE an item - restricted to users with the "admin" role //
 ///////////////////////////////////////////////////////////////
 router.post("/", isAuthorized, isAdmin, async (req, res) => {
-  const { title, price } = req.body;
-
   try {
-    const item = new Item({ title, price });
-    await item.save();
+    const item = await itemDao.createItem(req.body);
     res.status(201).json(item);
   } 
   // If it fails to create the item, return an error:
@@ -28,7 +26,7 @@ router.post("/", isAuthorized, isAdmin, async (req, res) => {
 ////////////////////////////////////////////
 router.get("/", isAuthorized, async (req, res) => {
   try {
-    const items = await Item.find();
+    const items = await itemDao.getAllItems();
     res.json(items);
   }
 
@@ -44,7 +42,7 @@ router.get("/", isAuthorized, async (req, res) => {
 ///////////////////////////////////////////////////////
 router.get("/:id", isAuthorized, async (req, res) => {
   try {
-    const item = await Item.findById(req.params.id);
+    const item = await itemDao.getItemById(req.params.id);
 
     // If the item doesn't exist, return an error:
     if (!item) return res.sendStatus(404);
@@ -63,16 +61,36 @@ router.get("/:id", isAuthorized, async (req, res) => {
 ///////////////////////////////////////////////////////////////
 router.put("/:id", isAuthorized, isAdmin, async (req, res) => {
   try {
-    const item = await Item.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    const item = await itemDao.updateItem(req.params.id, req.body);
 
     // If the requested item can't be found, return an error:
     if (!item) return res.sendStatus(404);
+
     // Otherwise, return the item in the response:
     res.json(item);
   }
   // If the update fails for some other reason, return an error:
   catch (err) {
     res.sendStatus(400);
+  }
+});
+
+
+/////////////////////////////////////////////////////////////////
+// DELETE an item - restricted to users with the "admin" role //
+///////////////////////////////////////////////////////////////
+router.delete("/:id", isAuthorized, isAdmin, async (req, res) => {
+  try {
+    const deleted = await itemDao.deleteItem(req.params.id);
+
+    // If the item can't be found, return a 404 error:
+    if (!deleted) return res.sendStatus(404);
+
+    // Otherwise, return confirmation that the item was deleted:
+    res.json({ message: "Item deleted" });
+  }
+  catch (error) {
+    res.sendStatus(500);
   }
 });
 
