828fb5f5fa38302e6713bf313e3b6a4e6bf27d7a
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-05-12 20:58:40 -0700

    Fixes export line error to be singular Item instead of plural items

--- Class5/models/itemModel.js
+++ Class5/models/itemModel.js
@@ -1,8 +1,17 @@
+// Import Mongoose:
 const mongoose = require("mongoose");
 
+///////////////////////////////////
+// Create a schema for an item: //
+/////////////////////////////////
 const itemSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  price: { type: Number, required: true },
+
+  // A title must be provided for each item and any surrounding white space will be trimmed away:
+  title: { type: String, required: true, trim: true },
+
+  // A price must be provided for each item and it can't be a negative number:
+  price: { type: Number, required: true, min: 0 },
 });
 
-module.exports = mongoose.model("items", itemSchema);
+
+module.exports = mongoose.model("Item", itemSchema);
