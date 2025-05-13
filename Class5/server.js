f7ad45e8abb90e83c2c9049b1ba4c3abd136ca25
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-05-13 01:01:16 -0700

    Adds missing route paths and mongoose connection

--- Class5/server.js
+++ Class5/server.js
@@ -1,10 +1,30 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const routes = require("./routes");
+// const dotenv = require(".env");
+const authRoutes = require("./routes/auth");
+const itemRoutes = require("./routes/items");
+const orderRoutes = require("./routes/orders");
 
 const server = express();
-server.use(express.json());
 
-server.use(routes);
+// dotenv.config();
+
+server.use(express.json());
+server.use("/auth", authRoutes);
+server.use("/items", itemRoutes);
+server.use("/orders", orderRoutes);
+
+
+// Connect to MongoDB:
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("✅ Connected to MongoDB"))
+  .catch((err) => {
+    console.error("❌ Failed to connect to MongoDB due to the following error:", err);
+  });
 
 module.exports = server;
