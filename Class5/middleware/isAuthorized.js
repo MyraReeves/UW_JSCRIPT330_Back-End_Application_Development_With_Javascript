7b59411ec601e6491b1ea8bad9b4263712fe6444
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-05-12 20:40:34 -0700

    Adds requirement that authorization header must begin with the word 'Bearer'

--- Class5/middleware/isAuthorized.js
+++ Class5/middleware/isAuthorized.js
@@ -1,24 +1,40 @@
-// INSTRUCTIONS:  You'll want to write an `isAuthorized` middleware function that can be reused. It should verify the JWT provided in `req.headers.authorization` and put the decoded value on the `req` object.
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// INSTRUCTIONS:  You'll want to write an `isAuthorized` middleware function that can be re-used.     //
+// It should verify the JWT provided in `req.headers.authorization`                                  //
+// and put the decoded value on the `req` object.                                                   //
+/////////////////////////////////////////////////////////////////////////////////////////////////////
 
+
+// Import the JSON web token library dependency:
 const jwt = require("jsonwebtoken");
 
+
+/////////////////////////////////////////
+// Middleware authorization function: //
+///////////////////////////////////////
 const isAuthorized = (req, res, next) => {
-  const authorizationHeader = req.headers.authorization;
+    const authorizationHeader = req.headers.authorization;      // Retrieves the value of the `Authorization` header from the incoming HTTP request.
 
-  if (!authorizationHeader) {
-    return res.sendStatus(401);
-  }
+    // If the header is missing or does not start with the word `Bearer ` return a 401 error:
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.sendStatus(401);         // Not authorized because no token was provided
+    }
 
-  const token = authorizationHeader.split(' ')[1];
+    // Extract the token from the header by looking for what follows the space after the word "Bearer":
+    const token = authorizationHeader.split(' ')[1];
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
-    next();
-  }
-  catch (error) {
-    return res.sendStatus(401);
-  }
+    // Attempt to verify and decode the token using the secret stored in the `.env` file (`JWT_SECRET`):
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    }
+
+// If an invalid token is given, return a 401 error:
+    catch (error) {
+        return res.sendStatus(401);
+    }
 };
+
 
 module.exports = isAuthorized;
