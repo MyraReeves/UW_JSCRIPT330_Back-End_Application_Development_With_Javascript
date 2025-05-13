d166826332acfc13970b460820152fb7c3af2bb8
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-05-12 21:38:54 -0700

    Fixes export to singular 'User' instead of plural 'users' and defines default 'user' role

--- Class5/models/userModel.js
+++ Class5/models/userModel.js
@@ -1,9 +1,18 @@
+// Import Mongoose:
 const mongoose = require("mongoose");
 
+//////////////////////////////////////////////
+// Define the schema for a user's account: //
+////////////////////////////////////////////
 const userSchema = new mongoose.Schema({
+  // A password is required
   password: { type: String, required: true },
-  email: { type: String, unique: true, required: true },
-  roles: { type: [String], required: true },
+
+  // Email is required, no duplicate emails are allowed, surrounding white space should be removed from the provided email, and all emails should be converted to all lowercase letter:
+  email: { type: String, unique: true, required: true, trim: true, lowercase: true },
+
+  // There is an array of roles, each user is required to have a role assigned to them, and the default role is "user":
+  roles: { type: [String], required: true, default: ["user"] },
 });
 
-module.exports = mongoose.model("users", userSchema);
+module.exports = mongoose.model("User", userSchema);
