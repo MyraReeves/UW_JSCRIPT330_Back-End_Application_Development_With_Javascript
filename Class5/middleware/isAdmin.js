07227bbe207cc71712e1a912075199f5ccbeda3a
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-05-14 23:37:26 -0700

    Adds  notes

--- Class5/middleware/isAdmin.js
+++ Class5/middleware/isAdmin.js
@@ -4,9 +4,11 @@
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
 const isAdmin = (req, res, next) => {
-    if (req.user && req.user.roles.includes("admin")) {
+    // Before granting access, check who the user is, their role, and that their role includes "admin":
+    if (req.user && req.user.roles && req.user.roles.includes("admin")) {
       return next();
     }
+    // If they are not an Admin, return a 403 "Forbidden" error:
     return res.sendStatus(403);
   };
   
