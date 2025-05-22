a34f1d4842ad4d1d24f5aeebe7111091b765c4b8
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-05-22 05:30:54 -0700

    Imports routes

--- Class7/server.js
+++ Class7/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
-
 const routes = require("./routes");
-
+const landingRoutes = require('./routes/landing');
+const weatherRoutes = require('./routes/weather');
 const server = express();
-server.use(express.json());
 
+
+server.use(express.json());
 server.use(routes);
+server.use('/landing', landingRoutes);
+server.use('/weather', weatherRoutes);
 
 module.exports = server;
